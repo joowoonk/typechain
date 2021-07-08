ce863b2ecbc0ebe514ab58c559c843541b78eec8
Your Name <joowoonk@gmail.com>
2021-07-07 18:10:20 -0700

    used interface for the first time

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-alert("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "John",
+    age: 28,
+    gender: "male"
+};
+const sayHi = (person) => {
+    return (`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
+};
+// const sayHi = (name:string, age:number, gender:string): string =>{
+//     return(`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
+// }
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-alert("hello")
+interface Human{
+    name:string;
+    age:number;
+    gender: string;
+}
+
+const person = {
+    name:"John",
+    age: 28,
+    gender: "male"
+}
+
+const sayHi = (person: Human): string =>{
+    return(`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
+}
+
+// const sayHi = (name:string, age:number, gender:string): string =>{
+//     return(`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
+// }
+
+console.log(sayHi(person))
+
+// console.log(sayHi("John",28,"male"))
+
+export {};
