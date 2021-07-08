02886d867b09ef4436096617b574d4a131129f59
Your Name <joowoonk@gmail.com>
2021-07-07 18:21:36 -0700

    created dummy data for blockchain (not real chain)

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 "use strict";
-// interface Human{
-//     name:string;
-//     age:number;
-//     gender: string;
-// }
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const lynn = new Human("Lynn", 18, "female");
-const person = {
-    name: "John",
-    age: 28,
-    gender: "male"
-};
-const sayHi = (person) => {
-    return (`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
-};
-// const sayHi = (name:string, age:number, gender:string): string =>{
-//     return(`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
-// }
-console.log(sayHi(lynn));
+const genesisBlock = new Block(0, "152313213213", "", "Hello", 213145);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,38 +1,28 @@
-// interface Human{
-//     name:string;
-//     age:number;
-//     gender: string;
-// }
-
-class Human {
-    public name:string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age:number, gender:string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-const lynn = new Human("Lynn", 18, "female")
+const genesisBlock: Block = new Block(0, "152313213213", "", "Hello", 213145);
 
-const person = {
-    name:"John",
-    age: 28,
-    gender: "male"
-}
+let blockchain: [Block] = [genesisBlock];
 
-const sayHi = (person: Human): string =>{
-    return(`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
-}
-
-// const sayHi = (name:string, age:number, gender:string): string =>{
-//     return(`hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
-// }
-
-console.log(sayHi(lynn))
-
-// console.log(sayHi("John",28,"male"))
+console.log(blockchain);
 
 export {};
